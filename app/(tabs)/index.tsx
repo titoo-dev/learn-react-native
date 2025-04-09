6198941f2f434bad9ce21890a716c1072b15534b
Titosy Manankasina <dev.titosy@gmail.com>
2025-04-09 16:07:46 +0300

    Add image picker functionality and button component; update ImageViewer to support selected images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,46 @@
 import { View } from 'react-native';
 import { StyleSheet } from 'react-native';
 import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+	const [selectedImage, setSelectedImage] = useState<string | undefined>(
+		undefined
+	);
+
+	const pickImageAsync = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			quality: 1,
+		});
+
+		if (!result.canceled) {
+			setSelectedImage(result.assets[0].uri);
+		} else {
+			alert('You did not select any image.');
+		}
+	};
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.imageContainer}>
-				<ImageViewer imgSource={PlaceholderImage} />
+				<ImageViewer
+					imgSource={PlaceholderImage}
+					selectedImage={selectedImage}
+				/>
+			</View>
+			<View style={styles.footerContainer}>
+				<Button
+					onPress={pickImageAsync}
+					theme="primary"
+					label="Choose a photo"
+				/>
+				<Button label="Use this photo" />
 			</View>
 		</View>
 	);
@@ -24,5 +56,9 @@ const styles = StyleSheet.create({
 	},
 	imageContainer: {
 		flex: 1,
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center',
 	},
 });
